f18d8b621dc608f1ffec46bba051a6594ab3a1f3
Philipp Gernandt <p_s_g88@yahoo.de>
2015-02-23 23:48:10 +0100

    added and changed some python-files for first outdoor-driving test with GPS-data! EXCITED!

--- pseudocode.py
+++ pseudocode.py
@@ -11,5 +11,31 @@ drive.left90(current_status, desired_status, speed='middle')
 drive.turn180(current_status, desired_status, speed='middle')
 
 
+log.py
+
+add_log(current_status, GPS_data) # saves all neccessary data to file. see log.py for details
+
 
 gpsData.py      # Max: made edit which should output important data. Not sure though.
+		# Phil: corrected that one. should work now
+
+
+direction.py
+
+get_direction(GPS_destination, GPS_data, desired_status) # Function which shall calculate the desired direction
+		#NOTE: get_direction maybe needs to be modified including current_status in order to drive better curves....
+
+
+distance_target.py
+
+get_target_distance(lat1, long1, lat2, long2) # calculates distance between two points on earth 
+					      # (accuracy unknown, does not use altitude, assumes perfect sphere)
+distance_on_unit_sphere(lat1, long1, lat2, long2) # calculates distance of two points on unit-sphere
+
+
+init.py
+
+init() # for initialization (what would we need??)
+
+
+main.py	# overall function to test everything
