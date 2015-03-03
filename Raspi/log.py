387529ac37ea3fa6a5d6abcd96a304dbfc7fd33a
Philipp Gernandt <p_s_g88@yahoo.de>
2015-03-03 13:58:16 +0100

    fixed wrong log-format

--- Raspi/log.py
+++ Raspi/log.py
@@ -19,23 +19,28 @@ import time	# maybe needs to be in add_log??
 class log:
 	def __init__(self, log_file_name):
 		self.log_file=open(log_file_name, 'a')
-		self.opened=True
+		self.opened = True
+		self.data_seperator = "\t"
 
 	def add_log(self, current_status, GPS_data):
+		if (self.opened == False):
+			print "Error: No file opened to write to! Exit."
+			return
 		gps_log = ''
 		for entry in GPS_data:
-			gps_log = gps_log + ' ' + str(entry) # concentrate GPS-data to string with spaces as separation
+			gps_log = gps_log + str(entry) + self.data_seperator # concentrate GPS-data to string with spaces as separation
 
 		status_log = ''
 		for entry in current_status:
-			status_log = status_log + ' ' + entry # concentrate current status to string with spaces as separation
+			status_log = status_log + entry + self.data_seperator # concentrate current status to string with spaces as separation
 
 		lt = time.localtime() # get local time information
-		current_time = time.strftime("%Y %m %d %H %M %S", lt) # concentrate local time to string
+		current_time = time.strftime("%Y" + self.data_seperator + "%m" + self.data_seperator + "%d" + self.data_seperator + "%H" + self.data_seperator + "%M" + self.data_seperator + "%S" + self.data_seperator, lt) # concentrate local time to string
 
-		log_entry = str(time.time()) + ' ' + current_time + gps_log + status_log # generate output string with all data (spaces for seperation)
+		log_entry = str(time.time()) + self.data_seperator + current_time + gps_log + status_log # generate output string with all data (spaces for seperation)
 		self.log_file.write(log_entry + '\n') # write output
 		return log_entry # return output
 
 	def stop(self):
 		self.log_file.close()
+		self.opened = False
